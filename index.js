c49d115888b6371dce9584fafc3d7a415d4b1308
Taher Khan <tkhan6@gmail.com>
2017-10-29 20:29:15 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -24,7 +24,11 @@ function sayHiToGrandma(string)
   {
     return ("I can't hear you!")
   }
-  else if (string === string.toUpperCase)
+}
+
+function sayHiToGrandma(string)
+{
+  if (string === string.toUpperCase())
   {
     return ("YES INDEED!")
   }
